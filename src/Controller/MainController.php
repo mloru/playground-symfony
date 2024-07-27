d187e8041ab97b65f0d7f957de5ebafa5d743ecd
Mirko Lorusso <mirko.lorusso@gmail.com>
2024-07-27 17:04:13 +0200

    Add route and controller that return a string

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+    #[Route("/home", "homepage")]
+    public function homepage(): Response
+    {
+        return new Response('<strong>Hello</strong> World!');
+    }
+}
